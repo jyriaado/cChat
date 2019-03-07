9eedd62ef35bc98f3c65b94d2601e6bb2b404eaf
jyrgen <jyrgen.niinre@telia.ee>
2019-03-07 20:07:23 +0200

    commit 2

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # cChat
 chat messaging app implementation for Network Protocol Design course
+
+Syntax:
+ .\hello.py  -server
+  Starts a server listener
+ .\hello.py  -client <ip> <port> <message>
+  Sends a message to ip:port
+
+Sample client usage:
+python .\hello.py -client 80.235.40.119 5005 hello2
--- hello.py
+++ hello.py
@@ -1,1 +1,50 @@
-print ("Hello World")
+
+import sys
+import socket
+
+
+def server(ip, port):
+
+    sock = socket.socket(socket.AF_INET, # Internet
+        socket.SOCK_DGRAM) # UDP
+    sock.bind((ip, int(port)))
+
+    while True:
+        data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+        print ("received message:", data.decode(), "addr:", addr)
+        #send back echo
+        sock.sendto(data, addr)
+        print ("sent message:", data.decode(), "addr:", addr)
+
+def client_send(ip, port, message, timeout):
+
+    print ("UDP target IP:", ip)
+    print ("UDP target port:", port)
+    print ("message:", message)
+ 
+    sock = socket.socket(socket.AF_INET, # Internet
+        socket.SOCK_DGRAM) # UDP
+    sock.sendto(message.encode(), (ip, int(port)))
+
+    #now wait 30s for an answer
+    print ("waiting for an answer on:",sock.getsockname())
+    sock.settimeout(timeout)
+    data, addr = sock.recvfrom(1024)
+    print ("received message:", data.decode(), "addr:", addr)
+
+def help():
+    print ("Syntax:")
+    print ("",sys.argv[0]," -server")
+    print ("  Starts a server listener")
+    print ("",sys.argv[0]," -client <ip> <port> <message> ")
+    print ("  Sends a message to ip:port")
+
+#print ("length:",str(len(sys.argv)))
+#print ("argv:",sys.argv)
+
+if (len(sys.argv)==2 and sys.argv[1] == "-server"):
+    server("127.0.0.1", "5005")
+elif (len(sys.argv)==5 and sys.argv[1] == "-client"):
+    client_send(sys.argv[2], sys.argv[3], sys.argv[4],30)
+else:
+    help()
